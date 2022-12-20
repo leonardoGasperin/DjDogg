f1da8a82ce6643d1bf3d96ac55c3830d64cdaecc
LeonardoGasperin <gasperinlvg@gmail.com>
2022-12-20 14:05:14 -0300

    preparing the app to revice navigation

---
+++ app/screens/AudioList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import { commonStyles } from '../../styles/CommonStyles';
+
+export function screens() {
+  return (
+    <View style={commonStyles.container}>
+
+    </View>
+  );
+}
